2bc3a60b0199184145bf823a7355ecde869f816b
Mekhribon <47243327+Mekhribon@users.noreply.github.com>
2019-04-30 18:50:54 +0300

    Add files via upload

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +12,6 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
-
     /**
      * Register any authentication / authorization services.
      *
@@ -23,8 +19,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Gate::define('update-post', function($user, $post){
+            return $user->id == $post->user_id;
+        });
         $this->registerPolicies();
-
         //
     }
 }
